3eabc3f25c89c7ebfc3ab3874c9ca8e5e4fb99fa
Gursharan Singh <gursharansinghsood@gmail.com>
2026-06-07 10:45:16 +0530

    Update api.js

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}api`,
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 // Add token to requests
